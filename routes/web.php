6e15a9302956b938a6b0f4b35b055b918829991c
Nouzi <tomikitd@gmail.com>
2024-10-18 16:16:23 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard', [
+      'text' => 'Hello World'
+    ]);
+});
+
+require __DIR__.'/auth.php';
